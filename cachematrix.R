e9ef97b6ec4251464515f78eca2fdf350d02e237
CMMNelson <chrystal.nelson@bankofengland.co.uk>
2017-05-19 16:41:35 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-## These functions cache the inverse of a matrix.
+## I have written a pair of functions that cache the inverse of a matrix.
 
 
 ## Write a short comment describing this function
